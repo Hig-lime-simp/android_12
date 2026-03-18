85499a5d4aa8494405006e0b45f146e43596de1b
lime <nikitagroshev11@gmail.com>
2026-03-18 20:24:40 +0300

    Step 7: Basic Grade Clicker app completed

--- app/src/main/java/com/example/grade_clicker/MainActivity.kt
+++ app/src/main/java/com/example/grade_clicker/MainActivity.kt
@@ -1,26 +1,168 @@
 package com.example.grade_clicker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.grade_clicker.ui.theme.Grade_ClickerTheme
+import androidx.compose.ui.unit.dp
+import com.example.grade_clicker.data.Datasource
+import com.example.grade_clicker.model.Grade
+import com.example.grade_clicker.ui.theme.GradeClickerTheme
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Grade_ClickerTheme {
+        Log.d(TAG, "onCreate Called")
 
+        setContent {
+            GradeClickerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GradeClickerApp(grades = Datasource().loadGrades())
+                }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+}
+
+@Composable
+fun GradeClickerApp(grades: List<Grade>) {
+    var points by rememberSaveable() { mutableIntStateOf(0) }
+    var clicks by rememberSaveable { mutableIntStateOf(0) }
+
+    val currentGrade = determineGradeToShow(grades, points)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        // Заголовок
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        // Кликабельное изображение
+        Image(
+            painter = painterResource(id = currentGrade.imageId),
+            contentDescription = null,
+            modifier = Modifier
+                .size(dimensionResource(id = R.dimen.image_size))
+                .clickable{
+                    points += currentGrade.pointsPerClick
+                    clicks++
+                },
+            contentScale = ContentScale.Fit
+        )
+
+        // Информация
+        TransactionInfo(points = points, clicks = clicks)
+    }
+}
+
+@Composable
+fun TransactionInfo(points: Int, clicks: Int) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Строка с баллами
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "${stringResource(id = R.string.points_earned)}: $points",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Строка с кликами
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "${stringResource(id = R.string.total_clicks)}: $clicks",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
+fun determineGradeToShow(grades: List<Grade>, points: Int): Grade {
+    var gradeToShow = grades.first()
+    for (grade in grades) {
+        if (points >= grade.threshold) {
+            gradeToShow = grade
+        } else {
+            break
+        }
+    }
+    return gradeToShow
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GradeClickerPreview() {
+    GradeClickerTheme {
+        GradeClickerApp(grades = Datasource().loadGrades())
     }
 }
